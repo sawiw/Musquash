0af891119a37ba4be876fce792c63cd4de14ee66
sawiw <96121984+sawiw@users.noreply.github.com>
2022-04-11 21:57:29 +0200

    a

---
+++ dossierIncludes/footer.php
@@ -0,0 +1,23 @@
+<footer>
+    <div class="footer-text">
+      <h2>Musquash ®</h2>
+      <p>Texte à ajouter</p>
+    </div>
+    <div class="footer-liens">
+        <h2>Liens utiles</h2></br>
+      <ul>
+        <li> <a href="#"> S'inscrire </a>
+        </li>
+        <li> <a href="#"> Se connecter </a></li>
+        <li> <a href="#"> Squash </a></li>
+        <li> <a href="#"> Reservations </a></li>
+
+      </ul>
+    </div>
+    <div class="footer-infos">
+      <h2>Musquash</h2></br>
+      Adresse :</br>
+      Mail :</br>
+      Numéro de téléphone :</br>
+    </div>
+  </footer>
--- dossierIncludes/indexNonCo.php
+++ dossierIncludes/indexNonCo.php
@@ -299,31 +299,9 @@
     </div>
   </div>
 
-
-  <footer>
-    <div class="footer-text">
-      <h2>Musquash ®</h2>
-      <p>Texte à ajouter</p>
-    </div>
-    <div class="footer-liens">
-      <ul>
-        <h2>Liens utiles</h2></br>
-        <li> <a href="#"> S'inscrire </a>
-        </li>
-        <li> <a href="#"> Se connecter </a></li>
-        <li> <a href="#"> Squash </a></li>
-        <li> <a href="#"> Reservations </a></li>
-
-      </ul>
-    </div>
-    <div class="footer-infos">
-      <h2>Musquash</h2></br>
-      Adresse :</br>
-      Mail :</br>
-      Numéro de téléphone :</br>
-    </div>
-  </footer>
-
+<?php
+include "footer.php";
+?>
 
 
 </body>
